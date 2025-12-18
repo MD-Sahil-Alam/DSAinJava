b69e752b20055beb391fae8531f61162bb76808c
MD-Sahil-Alam <mdsahil.prof@gmail.com>
2025-12-18 22:48:12 +0530

    Add GuessGame.java

---
+++ GuessGame.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class GuessGame {
+    public static void main(String[] args){ 
+      Scanner in = new Scanner(System.in);
+      
+      boolean correct = false;
+      int guess;
+      int rand = (int)(Math.random()*100) + 5;
+
+      while (correct == false) {
+          System.out.println("guess");
+          guess = in.nextInt();
+        if (guess == rand) {
+          System.out.println("Congract" +rand);
+          correct = true;
+        } else if (rand > guess) {
+          System.out.println("your guess is smaller than correct number");
+        }else {
+          System.out.println("your guess is heigher than correct number");
+        }
+      }
+    in.close();
+    }
+}
