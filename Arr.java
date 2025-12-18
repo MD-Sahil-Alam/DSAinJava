6c90908c51b39fb5d8e47f801c0a6b1f78ea931e
MD-Sahil-Alam <mdsahil.prof@gmail.com>
2025-12-18 22:45:28 +0530

    Add Arr.java

---
+++ Arr.java
@@ -0,0 +1,21 @@
+class Arr {
+  public static void main(String[] args) {
+    int arr[][] = {{1,3,5,7},{2,4,6,8}};
+
+    System.out.println("using for loop");
+    for(int row = 0; row < arr.length;row++){
+      for (int col = 0; col < arr[row].length;col++) {
+          System.out.print(arr[row][col]);
+      }
+      System.out.println("");
+    }
+
+    System.out.println("using for each");
+    for(int[] row : arr){
+      for (int num : row) {
+        System.out.print(num);
+      } 
+       System.out.println("");
+    }
+  }
+}
