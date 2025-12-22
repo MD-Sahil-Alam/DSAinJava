9562167667a8af00e637e1a1f8b3195c6f4ccd46
md-sahil-alam <mdsahil.prof@gmail.com>
2025-12-22 22:50:56 +0530

    add array swap

---
+++ Array/Swap.java
@@ -0,0 +1,17 @@
+package Array;
+
+import java.util.Arrays;
+
+public class Swap{
+  public static void main(String[] args) {
+    int[] arr = {10,20,30,40};
+    swap(arr,0,3);
+    System.out.println(Arrays.toString(arr));
+  }
+    static void swap(int[] arr,int a ,int b){
+    int temp = arr[a];
+    arr[a]= arr[b];
+    arr[b]=temp;
+    }
+}
+
