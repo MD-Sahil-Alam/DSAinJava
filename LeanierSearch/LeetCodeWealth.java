377168cf91a0a728893d6b5c8b9dcb913c346155
md-sahil-alam <mdsahil.prof@gmail.com>
2025-12-23 19:52:56 +0530

    leetcode wealth problem

---
+++ LeanierSearch/LeetCodeWealth.java
@@ -0,0 +1,18 @@
+package LeanierSearch;
+
+public class LeetCodeWealth {
+  public static void main(String[] args) {
+    int[][] nums = {
+            {1,2,3},
+            {3,2,1}
+    };
+
+    for(int i = 0 ; i< nums.length; i++){
+      int Wealth = 0;
+      for (int n : nums[i]) {
+           Wealth+=n;
+      }
+      System.out.println("Wealthof person "+" "+i+" = "+Wealth);
+    }
+  } 
+}
