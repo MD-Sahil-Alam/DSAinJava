0dcf643382f10981a5e6e34c2021eee6119896f2
md-sahil-alam <mdsahil.prof@gmail.com>
2025-12-24 18:38:12 +0530

    leet code smallesLetterGreaterThanTarget

---
+++ BinarySearch/GetsmallesLetterGreaterThanTarget.java
@@ -0,0 +1,23 @@
+package BinarySearch;
+
+public class GetsmallesLetterGreaterThanTarget {
+  public static void main(String[] args) {
+    char[] letter ={'c','f','j'};
+    char target = 'f';
+    System.out.println(find(letter, target));
+  }
+  static char find(char[] letter, char target){
+      int start = 0;
+      int end = letter.length-1;
+      while (start<=end) { 
+        int mid = start+(end-start)/2;
+      if(target<letter[mid]){
+          end = mid-1;
+        }else{
+          start = mid+1;
+        }
+      }
+      return letter[start % letter.length];
+    }
+  }
+
