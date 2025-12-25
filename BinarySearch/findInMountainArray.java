b36ab8457e433b354ff22a518a7e0564d5a87027
md-sahil-alam <mdsahil.prof@gmail.com>
2025-12-26 02:34:54 +0530

    add findInMountainArray hard  leet code

--- BinarySearch/findInMountainArray.java
+++ BinarySearch/findInMountainArray.java
@@ -1,49 +1,69 @@
 package BinarySearch;
 
-
-public class findInMountainArray {
+public class findInMountainArray{
   public static void main(String[] args) {
     int[] arr = {1,2,3,4,5,3,1};
     int target = 3;
-    System.out.println(find(arr,target)); 
-  }
 
-  static int find(int [] arr, int target){
+    //fist find max in the arr
+    int max = -1;
     int ans = -1;
     int start = 0;
     int end = arr.length-1;
-    while (start<end) { 
-        int mid = start+(end-start)/2;
-        if (arr[mid]==target) {
-          if (mid<ans) {
-            ans =  mid;
-            
-          } 
-        }else{
-            ans = asearch(arr,target,mid);
-            System.out.println(ans);
-            break;
-        }
+    while(start < end ){
+      int mid = start+(end-start)/2;
+      if (arr[mid]>arr[mid+1]) {
+          end = mid;
       }
-    return ans;
-  }
-
-  static int asearch(int[] arr , int target, int end){
-    int ans = -1;
-    int start = 0;
-    while (start<end) { 
-        int mid = start+(end-start)/2;
-        if (arr[mid]==target) {
-          if (mid<ans) {
-            System.out.println(mid);
-            return mid;
-          } 
-    }else if (arr[mid]>target) {
-      end = mid-1;
+      else{
+        start = mid+1;
+      }
+    }
+    max = start;
+  
+    if (arr[max]==target) {
+        ans = max;
     }else{
-      start = mid + 1;
+      int a = asearch(arr, target, max-1);
+      int d = dsearch(arr, target, max+1);
+      System.out.println(giveans(a,d));
     }
   }
-  return -1;
-}
+
+  static int giveans(int a , int d){
+    if (a != -1) {
+        return a;
+    }
+    return d;
+  }
+  
+  static int asearch(int [] arr ,int target ,int end){
+    int start = 0;
+    while (start<=end) { 
+        int mid = start+(end-start)/2;
+        if (arr[mid]==target) {
+          return mid;
+        }else if (arr[mid]<target) {
+          start = mid+1;
+        }else{
+          end = mid-1;
+        }
+    }
+    return -1;
+  }
+
+    static int dsearch(int [] arr ,int target ,int start){
+    int end = arr.length-1;
+    while (start<=end) { 
+        int mid = start+(end-start)/2;
+        if (arr[mid]==target) {
+          return mid;
+        }else if (arr[mid]>target) {
+          start = mid+1;
+        }else{
+          end = mid-1;
+        }
+    }
+    return -1;
+  }
 }
