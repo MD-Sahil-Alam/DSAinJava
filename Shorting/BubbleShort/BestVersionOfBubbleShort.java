77b9906d1fd10ef1db254ea77272902209cfa165
md-sahil-alam <mdsahil.prof@gmail.com>
2025-12-29 00:02:07 +0530

    best version or bubble short

---
+++ Shorting/BubbleShort/BestVersionOfBubbleShort.java
@@ -0,0 +1,32 @@
+package Shorting.BubbleShort;
+
+import java.util.Arrays;
+
+public class BestVersionOfBubbleShort {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 5, 4, 2};
+        int end = arr.length - 1;
+        boolean swapped;
+
+        while (end > 0) {
+            swapped = false;
+
+            for (int i = 0; i < end; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+            end--;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
