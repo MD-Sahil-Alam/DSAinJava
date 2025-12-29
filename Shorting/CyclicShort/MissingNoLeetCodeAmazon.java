2b6bef730fd894d4a822404f5bd2515a596084a0
md-sahil-alam <mdsahil.prof@gmail.com>
2025-12-30 00:05:38 +0530

    amazon leet code cyclic short

--- Shorting/CyclicShort/MissingNoLeetCodeAmazon.java
+++ Shorting/CyclicShort/MissingNoLeetCodeAmazon.java
@@ -1,8 +1,28 @@
 package Shorting.CyclicShort;
 
+import java.util.Arrays;
+
 public class MissingNoLeetCodeAmazon {
 
     public static void main(String[] args) {
-
+        int[] arr = {2, 5, 4, 1, 3, 7};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
+
+    static void sort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+
+            if (arr[i] <= arr.length && arr[i] != arr[correct]) {
+                int temp = arr[i];
+                arr[i] = arr[correct];
+                arr[correct] = temp;
+            } else {
+                i++;
+            }
+        }
+    }
+
 }
