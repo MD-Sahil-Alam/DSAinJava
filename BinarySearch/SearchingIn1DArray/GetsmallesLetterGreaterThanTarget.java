b5a0098631c3611f0c8b976c71d6b50db897f597
md-sahil-alam <mdsahil.prof@gmail.com>
2025-12-31 00:24:54 +0530

    update

--- BinarySearch/SearchingIn1DArray/GetsmallesLetterGreaterThanTarget.java
+++ BinarySearch/SearchingIn1DArray/GetsmallesLetterGreaterThanTarget.java
@@ -1,23 +1,24 @@
 package BinarySearch.SearchingIn1DArray;
 
 public class GetsmallesLetterGreaterThanTarget {
-  public static void main(String[] args) {
-    char[] letter ={'c','f','j'};
-    char target = 'f';
-    System.out.println(find(letter, target));
-  }
-  static char find(char[] letter, char target){
-      int start = 0;
-      int end = letter.length-1;
-      while (start<=end) { 
-        int mid = start+(end-start)/2;
-      if(target<letter[mid]){
-          end = mid-1;
-        }else{
-          start = mid+1;
-        }
-      }
-      return letter[start % letter.length];
-    }
-  }
 
+    public static void main(String[] args) {
+        char[] letter = {'c', 'f', 'j'};
+        char target = 'f';
+        System.out.println(find(letter, target));
+    }
+
+    static char find(char[] letter, char target) {
+        int start = 0;
+        int end = letter.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < letter[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letter[start % letter.length];
+    }
+}
