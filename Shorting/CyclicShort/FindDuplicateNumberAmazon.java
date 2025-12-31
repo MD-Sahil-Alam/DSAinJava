fb940f6db17e220e9d9e1f0ea844c9bde215ae9b
md-sahil-alam <mdsahil.prof@gmail.com>
2026-01-01 01:21:57 +0530

    Microsoft cyclic find all duplicate

--- Shorting/CyclicShort/FindDuplicateNumberAmazon.java
+++ Shorting/CyclicShort/FindDuplicateNumberAmazon.java
@@ -10,10 +10,11 @@ public class FindDuplicateNumberAmazon {
     static int sort(int[] arr) {
         int i = 0;
         while (i < arr.length) {
-            if (arr[i] != i + 1) {
+            if (arr[i] != i + 1) {//checkig if elem of arr correct index or not if not finds it corrct index and tehen check if correct index have diffrent valu if yes swap and if not then it is duplicate elem
+
                 int correct = arr[i] - 1;
 
-                if (arr[i] != arr[correct]) {
+                if (arr[i] != arr[correct]) {//checking if correct index is not having the same 
                     int temp = arr[i];
                     arr[i] = arr[correct];
                     arr[correct] = temp;
